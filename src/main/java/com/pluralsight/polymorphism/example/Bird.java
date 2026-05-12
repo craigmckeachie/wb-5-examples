670b0cad1e3ad9266a6e7373edaad9dad9bf4cf4
Craig McKeachie <craigdmckeachie@gmail.com>
2026-05-12 12:21:59 -0400

    Add animals speak methods

--- src/main/java/com/pluralsight/polymorphism/example/Bird.java
+++ src/main/java/com/pluralsight/polymorphism/example/Bird.java
@@ -1,4 +1,10 @@
 package com.pluralsight.polymorphism.example;
 
 public class Bird extends Animal{
+
+    @Override
+    public void speak() {
+        System.out.println("Tweet, Tweet");
+    }
+
 }
--- src/main/java/com/pluralsight/polymorphism/example/Cat.java
+++ src/main/java/com/pluralsight/polymorphism/example/Cat.java
@@ -1,4 +1,10 @@
 package com.pluralsight.polymorphism.example;
 
 public class Cat extends Animal {
+
+    @Override
+    public void speak() {
+        System.out.println("Meow, Meow");
+    }
+
 }
